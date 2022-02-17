56484306ba5b6f278e74493beaa02e8e8b98f868
Delphine <delphine.pva@gmail.com>
2022-02-17 13:12:17 +0100

    Add mouvement on the ball

--- src/main/java/com/example/pong/HelloApplication.java
+++ src/main/java/com/example/pong/HelloApplication.java
@@ -15,98 +15,114 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+
 public class HelloApplication extends Application {
 
-    static int circleSize = 50;
+    static int stageHeight = 1000;
+    static int stageWidth = 1500;
 
+    static int recHeight = 200;
+    static int recWidth = 50;
+    static int rec1PosX = 50;
+    static int rec1PosY = 500;
+    static int rec2PosX = 1400;
+    static int rec2PosY = 500;
 
-    static int stageWidth = 1280;
-    static int stageHeight = 720;
-
-    static int recHeight = 25;
-    static int recWidth = 150;
-
-    static int rec1posX = 1;
-    static int rec1posY = 50;
-
-    static int rec2posX = 1254;
-    static int rec2posY = 325;
-
-
-    static int ballSize = 25;
-    static int ballPosX = 640;
-    static int ballPosY = 325;
+    static int ballSize = 20;
+    static int ball1PosX = 750;
+    static int ballPosY = 500;
 
     static int directionBallX = 1;
     static int directionBallY = 1;
 
     Timeline tl;
+
     Rectangle ball;
-    Rectangle r2;
     Rectangle r1;
+    Rectangle r2;
 
-    TextArea txt;
+    TextArea t1;
 
+    Text score1, score2;
+    int scoreInt1 = 0, scoreInt2 = 0;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Pong");
+        ball = new Rectangle(ball1PosX,ballPosY, ballSize, ballSize);
 
-        ball = new Rectangle(ballPosX, ballPosY, ballSize, ballSize);
-        r1 = new Rectangle(rec1posX, rec1posY, recHeight, recWidth);
-        r2 = new Rectangle(rec2posX, rec2posY, recHeight, recWidth);
-        txt = new TextArea(("text?");
-        txt.fontProperty()
+        r1 = new Rectangle(rec1PosX, rec1PosY, recWidth, recHeight);
+        r2 = new Rectangle(rec2PosX, rec2PosY, recWidth, recHeight);
+        t1 = new TextArea("text?");
+        score1 = new Text(100,100, String.valueOf(scoreInt1));
+        score2 = new Text(600,100, String.valueOf(scoreInt2));
+        score1.setStyle("-fx-font-size: 100");
+        score2.setStyle("-fx-font-size: 100");
 
-        Group group = new Group(ball, r1, r2);
-        Scene scene = new Scene(group, stageWidth, stageHeight);
 
-        stage.setTitle("Pong!");
-        stage.setScene(scene);
+        Group group = new Group(ball, r1, r2, score1, score2);
+        primaryStage.setScene(new Scene(group, stageWidth, stageHeight));
 
-        //tl = new Timeline();
+        // tl = new Timeline();
         tl = new Timeline(new KeyFrame(Duration.millis(100), e -> run()));
         tl.setCycleCount(Timeline.INDEFINITE);
-        stage.show();
 
-        // Move R1 by mouse event
+        //move R1 by mouse event
+//        primaryStage.
 
+
+        primaryStage.show();
         tl.play();
     }
 
-    public void run() {
+    public void run(){
 
-        if((ball.getX() + ball.getWidth() + 1 >= r2.getX()) && (ball.getY()>=r2.getY() && ball.getY()<=(r2.getY()+r2.getHeight()))
-                || (ball.getX()-1 <= r1.getX() + r1.getWidth()) && (ball.getY()>=r1.getY() && ball.getY()<=(r1.getY()+r1.getHeight())))
-        {
-            directionBallX=-directionBallX;
+
+
+        if(((ball.getX() + ball.getWidth() +1 >= r2.getX())
+                && (ball.getY()>=r2.getY() && ball.getY()<=(r2.getY()+r2.getHeight())))
+                ||
+                ((ball.getX()-1 <= r1.getX() + r1.getWidth())
+                        && (ball.getY()>=r1.getY() && ball.getY()<=(r1.getY()+r1.getHeight()))
+                )
+        ) {
+            directionBallX = -directionBallX;
         }
 
-        if(ball.getY()<=0 || ball.getY()+ ball.getWidth()>=stageHeight){
+        if(ball.getY()<=0 || ball.getY()+ball.getWidth()>=stageHeight){
             directionBallY = -directionBallY;
         }
 
-        ball.setX(ball.getX()+ball.getWidth() * directionBallX);
-        ball.setY(ball.getY()+ball.getHeight()/8 * directionBallY);
-        r2.setY(ball.getX());
+        if(ball.getX()<=0 || ball.getX()+ball.getWidth()>=stageWidth){
+            if(ball.getX()<=0){
+                scoreInt2 = ++scoreInt2;
+                score2.setText(String.valueOf(scoreInt2));
+            }
+
+        }
+
+        ball.setX(ball.getX()+ball.getWidth() * directionBallX) ;
+        ball.setY(ball.getY()+ball.getHeight() * directionBallY);
+        r2.setY(ball.getY());
         r1.setY(ball.getY());
 
         tl.getKeyFrames().addAll(
-                new KeyFrame(Duration.millis(5000),
+                new KeyFrame(Duration.millis(1000),
                         new KeyValue(ball.translateXProperty(), ball.getX() + ball.getWidth()),
                         new KeyValue(ball.translateYProperty(), ball.getY() + ball.getWidth()),
                         new KeyValue(r2.translateYProperty(), ball.getY()),
                         new KeyValue(r1.translateYProperty(), ball.getY())
 
                 )
-
         );
-
         tl.play();
     }
 
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
